3e6c191a2dc4630b7b460825b13a20cc7bf0f947
Konovalov Sergey <sskonovalov3@gmail.com>
2018-04-09 14:04:42 +0300

    Добавлена статистика

--- src/BuildGUI.java
+++ src/BuildGUI.java
@@ -1,29 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BuildGUI {
     JFrame startFrame;
-    Panel basePanel;
+    JPanel basePanel;
+    JButton startButton;
+    JButton exitButton;
 
     public BuildGUI(){
         startFrame = new JFrame("SeaBattle");
-        startFrame.setSize(new Dimension(700,400));
+        startFrame.setSize(new Dimension(800,400));
         startFrame.setVisible(true);
         startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
     public void buildingFormGUI() {
-        basePanel = new Panel();
-        basePanel.setBackground(Color.BLACK);
-        startFrame.add(basePanel);
-        basePanel.setLayout(new FlowLayout());
-        GameField playerField = new GameField();
-        GameField playerField1 = new GameField();
-        playerField.setBackground(Color.BLACK);
-        playerField1.setBackground(Color.RED);
-        basePanel.add(playerField);
-        basePanel.add(playerField1);
+
+        startButton = new JButton("START");
+        exitButton = new JButton("EXIT");
+        startFrame.add(startButton, BorderLayout.AFTER_LAST_LINE);
+
+
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startButton.setVisible(false);
+                basePanel = new JPanel();
+                basePanel.setBackground(Color.BLACK);
+                basePanel.setLayout(new FlowLayout());
+                GameField playerField = new GameField();
+                GameField playerField1 = new GameField();
+                startFrame.add(basePanel);
+                basePanel.add(playerField);
+                basePanel.add(StatusPanel.createStatusPanel());
+                basePanel.add(playerField1);
+                basePanel.add(exitButton);
+                exitButton.addActionListener((n) -> System.exit(0));
+
+            }
+        });
+
+
 
     }
 }
